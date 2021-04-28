689ce57a559f60ee4a784c6a060be32c4b2aa491
jespy2 <jespy2@gmail.com>
2021-04-28 12:41:11 -0500

    updating react file structure

---
+++ client/pages/Login.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from '../img/logo.svg';
+import './Login.css';
+
+//have login and password input field 
+//with "login" button and signup link
+
+const Login = props => {
+  let info = [];
+  if (props.info == 'wrongPassword') info.push(<div className="info">Wrong password!</div>)
+  if (props.info == 'unknownUser') info.push(<div className="info">Unknown user!</div>)
+  return (
+    <div className='loginsignuppage'>
+      <img className="Login-Logo" src={logo} alt="Multicommunicado" />
+      <div className="loginSignup">
+        <div className="loginSignupForm">
+          <h1>Login</h1>
+          <input className="loginInput" id="userLogin" type="text" placeholder="username" /><br />
+          <input className="loginInput" id="passLogin" type="password" placeholder="password" /><br />
+          {info}
+          <button className="loginSignupButton" onClick={props.submitLogin}>Login</button>
+          <button className="loginSignupButton" onClick={() => props.onSignUpClick(true)}>Sign up</button>
+        </div>
+      </div>
+    </div>
+
+  );
+}
+
+export default Login;
