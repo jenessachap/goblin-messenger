93bd2c0c819b93c67a40caa237a467790237a256
Jason Fricano <jasonfricano@Ninjas-MBP.local>
2021-04-28 19:35:21 -0400

    format to 2-space tabs in controller files

--- server/controllers/cookieController.js
+++ server/controllers/cookieController.js
@@ -1,33 +1,38 @@
 const cookieController = {};
-const { User } = require('./../models.js');
+const { User } = require("./../models.js");
 
 // store the user id in a cookie for auth
 cookieController.setSSIDCookie = (req, res, next) => {
   if (res.locals.rejectNewUser) return next();
 
-    console.log(`we're in the cookie controller now`);
-    console.log(req.cookies);
-    console.log(res.locals.user)
-    if (!res.locals.user) {
-        console.log('no user found! skipping cookie controller.')
-        return next();
-    }
-    res.cookie('ssid', res.locals.user._id, { httpOnly: true });
-    console.log(`This is the response for the set cookie from the db ${res.locals.user} `);
+  console.log(`we're in the cookie controller now`);
+  console.log(req.cookies);
+  console.log(res.locals.user);
+  if (!res.locals.user) {
+    console.log("no user found! skipping cookie controller.");
     return next();
+  }
+  res.cookie("ssid", res.locals.user._id, { httpOnly: true });
+  console.log(
+    `This is the response for the set cookie from the db ${res.locals.user} `
+  );
+  return next();
 };
 
 cookieController.findUserByCookie = (req, res, next) => {
-    User.findOne({_id: req.cookies.ssid})
-      .then((response) => res.locals.user = response)
-      .then(next())
-      .catch((err)=>{
-          if (err) return next({
-            log: `cookieController.findUserByCookie: ERROR: Did not properly find user by cookie`,
-            message: { err: 'cookieController.findUserByCookie: ERROR: Check server logs for details' }
-        })
-    })
-}
-
+  User.findOne({ _id: req.cookies.ssid })
+    .then((response) => (res.locals.user = response))
+    .then(next())
+    .catch((err) => {
+      if (err)
+        return next({
+          log: `cookieController.findUserByCookie: ERROR: Did not properly find user by cookie`,
+          message: {
+            err:
+              "cookieController.findUserByCookie: ERROR: Check server logs for details",
+          },
+        });
+    });
+};
 
 module.exports = cookieController;
--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,108 +1,92 @@
-const { User } = require('../models');
-
+const { User } = require("../models");
 
 const userController = {};
 
 userController.getAllUsers = (req, res, next) => {
-    try {
-        User.find({}, (err, users) => {
-            console.log(users)
-            res.locals.users = users;
-            return next();
-        });
-        
-    } catch (err) {
-        return next(`Error in userController.getAllUsers: ${JSON.stringify(err)}`);
-    }
+  try {
+    User.find({}, (err, users) => {
+      console.log(users);
+      res.locals.users = users;
+      return next();
+    });
+  } catch (err) {
+    return next(`Error in userController.getAllUsers: ${JSON.stringify(err)}`);
+  }
 };
 
 userController.findOneUser = (req, res, next) => {
-  console.log('req body', req.body);
+  console.log("req body", req.body);
 
   User.findOne({ username: req.body.username })
-    .then(data => {
-      console.log('data', data);
+    .then((data) => {
+      console.log("data", data);
       if (data) {
-        console.log('existing user:', data);
+        console.log("existing user:", data);
         res.locals.rejectNewUser = true;
-      }
-      else res.locals.rejectNewUser = false;
+      } else res.locals.rejectNewUser = false;
       return next();
     })
-    .catch(
-      err => next({
-          log: `error in userController.findOneUser: ${err}`,
-          message: {err: `error in findOneUser controller`}
+    .catch((err) =>
+      next({
+        log: `error in userController.findOneUser: ${err}`,
+        message: { err: `error in findOneUser controller` },
       })
     );
-}
+};
 
 userController.createUser = async (req, res, next) => {
-    console.log(req.body)
-    const { username, password, language } = req.body;
-    if (
-      username === '' ||
-      password === '' ||
-      language === ''
-    ) res.locals.rejectNewUser = true;
-    if (res.locals.rejectNewUser) return next();
+  console.log(req.body);
+  const { username, password, language } = req.body;
+  if (username === "" || password === "" || language === "")
+    res.locals.rejectNewUser = true;
+  if (res.locals.rejectNewUser) return next();
 
-    console.log('username, password', username, password)
-    // if(username === ''){
-    //     res.locals.noUsername = true;
-    //     res.json(res.locals);
-    // }
-    // if(password === ''){
-    //     res.locals.noPassword = true;
-    //     res.json(res.locals);
-    // }
-    // if(language === ''){
-    //     res.locals.noLanguage = true;
-    //     res.json(res.locals);
-    // }
+  console.log("username, password", username, password);
 
-
-    try {
-        console.log(`we're in the userController try statement`)
-        const newUser = await User.create({ username, password, language });
-        res.locals.user = newUser;
-        return next();
-    }   catch (err) {
-        User.findOne({ username }, (err, response) => {
-            if (username === response) {
-                return next(err, {
-                    log: `userController.createUser: ERROR: User might already exsist`,
-                    message: { err: 'userController.createUser: ERROR: User might already exsist.' }
-                });
-            }    
-        })
-    }    
+  try {
+    console.log(`we're in the userController try statement`);
+    const newUser = await User.create({ username, password, language });
+    res.locals.user = newUser;
+    return next();
+  } catch (err) {
+    User.findOne({ username }, (err, response) => {
+      if (username === response) {
+        return next(err, {
+          log: `userController.createUser: ERROR: User might already exsist`,
+          message: {
+            err: "userController.createUser: ERROR: User might already exsist.",
+          },
+        });
+      }
+    });
+  }
 };
 
 userController.verifyUser = (req, res, next) => {
-    console.log(`this is the verifyUser request username: ${req.body.username} password: ${req.body.password}`)
-    //send a req to the db and save the username & pw in res.local
-    const { username, password } = req.body;
+  console.log(
+    `this is the verifyUser request username: ${req.body.username} password: ${req.body.password}`
+  );
+  //send a req to the db and save the username & pw in res.local
+  const { username, password } = req.body;
 
-    // const username = req.body.username;
-    // const password = req.body.password;
+  // const username = req.body.username;
+  // const password = req.body.password;
 
-    try { 
-        User.findOne({ username }, (err, response ) => {
-            console.log(password)
-            console.log(`This is the verify user response from the db: ${response}`)
-            if (response === null) {
-                res.locals.userUnknown = true;
-            } else if (password === response.password) {
-                console.log(`User logged in as username: ${username}`)
-                res.locals.user = response;
-            } else res.locals.noMatch = true;
-            return next();
-            })
-        }
-    catch (err) {
-        return next(err);
-    }
+  try {
+    User.findOne({ username }, (err, response) => {
+      console.log(password);
+      console.log(`This is the verify user response from the db: ${response}`);
+      if (response === null) {
+        res.locals.userUnknown = true;
+      } else if (password === response.password) {
+        console.log(`User logged in as username: ${username}`);
+        res.locals.user = response;
+      } else res.locals.noMatch = true;
+      return next();
+    });
+  } catch (err) {
+    return next(err);
+  }
 };
 
 module.exports = userController;
